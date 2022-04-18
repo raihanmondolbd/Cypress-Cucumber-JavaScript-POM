d9def5b4f0cb6114b830baac671ba0555f7823b9
raihanmondolbd <raihanmondolbd@gmail.com>
2022-04-18 19:18:30 +0600

    Some modifications

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,9 +1,14 @@
 /// <reference types="cypress" />
 const cucumber = require('cypress-cucumber-preprocessor').default
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
+// module.exports = (on, config) => {
+//     on('file:preprocessor', cucumber())
+//     allureWriter(on, config);
+//     return config;
+// }
 
 module.exports = (on, config) => {
-    on('file:preprocessor', cucumber())
-    allureWriter(on, config);
-    return config;
+ on('file:preprocessor', cucumber())
+ allureWriter(on, config);
+  return config;
 }
